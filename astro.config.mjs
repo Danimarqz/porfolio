eea9510396be1c9860a415e667d09d837912dd51
Danimarqz <daniel.marquez@ciclosmontecastelo.com>
2024-08-23 19:21:11 +0200

    vercel deploy analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel/static'
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://danimarqz.dev/',
-  output: 'server',
+  output: 'static',
   adapter: vercel({
     webAnalytics: {enabled: true}
   })
