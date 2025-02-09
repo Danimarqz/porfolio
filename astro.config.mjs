ef202ed8d84c5e471ac154c632b0a3f02d7e3667
Danimarqz <daniel.marquez@ciclosmontecastelo.com>
2025-02-09 13:50:03 +0100

    feat -> Manual translations

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,21 @@ import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [
+    tailwind(),
+    robotsTxt(),
+  ],
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+    tranlsations: {
+      en: './src/i18n/en.json',
+      es: './src/i18n/es.json'
+    }
+  },
   site: 'https://danimarqz.dev/',
   output: 'static',
   adapter: vercel({
-    webAnalytics: {enabled: true}
+    webAnalytics: { enabled: true }
   })
 })
