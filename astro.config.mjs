cbd3e917c117773124f03ebb0f9733d6cac8cc51
Daniel Márquez <hola@danimarqz.dev>
2026-06-07 22:51:20 +0200

    migrate to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from "@tailwindcss/vite"
-import vercel from '@astrojs/vercel'
 import robotsTxt from "astro-robots-txt"
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     robotsTxt(),
   ],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   site: 'https://danimarqz.dev/',
   output: 'static',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  })
+  adapter: cloudflare(),
 })
